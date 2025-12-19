97d21acc45639616d65cbfef4e5cf06214a1b55d
devsarahgeo <devsarahgeo@gmail.com>
2025-12-19 12:27:27 -0500

    small changes

--- dbt-dag/dags/movie/models/analysis/movie_release_trends.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    EXTRACT(year FROM release_date) AS release_year,
-    COUNT(DISTINCT movie_id) AS movies_released
-FROM {{ ref('seed_movie_release_dates') }}
-GROUP BY release_year
-ORDER BY release_year ASC
--- dbt-dag/dags/movie/models/analysis/user_engagement.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    user_id,
-    COUNT(*) AS number_of_ratings,
-    AVG(rating) AS average_rating_given
-FROM {{ ref('fct_ratings') }}
-GROUP BY user_id
-ORDER BY number_of_ratings DESC
-LIMIT 20
